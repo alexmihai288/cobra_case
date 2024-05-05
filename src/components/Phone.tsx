c235bac02e7899d650d25e9bf3f6f8047cb3098b
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-05 22:36:48 +0300

    chore - user

---
+++ src/components/Phone.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+export const Phone = ({
+  imgSrc,
+  className,
+  dark = false,
+  ...props
+}: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <Image
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        className="pointer-events-none z-50 select-none"
+        alt="phone-image"
+        width={8000}
+        height={8000}
+      />
+      <div className="absolute -z-10 inset-0">
+        <Image
+          className="object-cover"
+          src={imgSrc}
+          alt="overlaying phone image"
+          width={8000}
+          height={8000}
+        />
+      </div>
+    </div>
+  );
+};
