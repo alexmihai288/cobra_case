7693b30b93becc32085ff6594296101cd414f08f
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-06 12:21:52 +0300

    chore auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,13 @@ import { MaxWidthWrapper } from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+export const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -65,7 +68,7 @@ export const Navbar = () => {
                   href="/configure/upload"
                   className={buttonVariants({
                     size: "sm",
-                    className: "hidden sm:flex items-center gap-1",
+                    className: "hidden sm:flex items-center gap-1 rounded-[8px]",
                   })}
                 >
                   Create case
