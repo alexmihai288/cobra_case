9396111a194918eb6064c6ad2686aaedac5d4ff8
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-06 12:36:37 +0300

    chore - testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/Icons";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import { Phone } from "@/components/Phone";
 import { Check, Star } from "lucide-react";
@@ -11,7 +12,12 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <Image src="/snake-1.png" alt="logo" width={8000} height={8000} />
+                <Image
+                  src="/snake-1.png"
+                  alt="logo"
+                  width={8000}
+                  height={8000}
+                />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on a{" "}
@@ -106,8 +112,118 @@ export default function Home() {
                 width={8000}
                 height={8000}
               />
-              <Image src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" width={8000} height={8000} alt="line"/>
-              <Phone className="w-64" imgSrc="/testimonials/1.jpg"/>
+              <Image
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+                width={8000}
+                height={8000}
+                alt="line"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-emerald-600" />
+              </span>
+              say
+            </h2>
+
+            <Image
+              src="/snake-2.png"
+              className="w-24 order-0 lg:order-2"
+              width={8000}
+              height={8000}
+              alt="snake logo"
+            />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;The case feels durable and I even got a compliment on
+                  the design. Had the case for two and a half months now and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after couple weeks. Love it.&quot;
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <Image
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                  width={8000}
+                  height={8000}
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-emerald-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+                <Star className="h-5 w-5 text-emerald-600 fill-emerald-600" />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;I usually keep my phone together with my keys in my
+                  pocket and that led to some pretty heavy scratchmarks on all
+                  of my last phone cases. This one, besides a barely noticeable
+                  scrath on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it.&quot;
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <Image
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                  width={8000}
+                  height={8000}
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Josh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-emerald-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
