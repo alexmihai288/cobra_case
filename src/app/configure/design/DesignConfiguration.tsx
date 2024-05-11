a3cf7184c80ff9cba96c60e089891261c25af09c
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-11 22:51:03 +0300

    chore - color options to be done

--- src/app/configure/design/DesignConfiguration.tsx
+++ src/app/configure/design/DesignConfiguration.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import React from "react";
+import React, { useState } from "react";
 import NextImage from "next/image";
 import { cn } from "@/lib/utils";
 import { Rnd } from "react-rnd";
 import { HandleComponent } from "@/components/HandleComponent";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { RadioGroup } from "@headlessui/react";
 
 interface DesignConfigurationProps {
   configId: string;
@@ -17,6 +19,10 @@ export const DesignConfiguration = ({
   imageUrl,
   imageDimensions,
 }: DesignConfigurationProps) => {
+  const [options,setOptions] = useState({
+    color:"black"
+  })
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:right-2 focus:ring-primary focus:ring-offset-2">
@@ -32,7 +38,7 @@ export const DesignConfiguration = ({
               className="pointer-events-none z-50 select-none"
             />
           </AspectRatio>
-          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229, 231,235,0.6)]" />
+          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
 
           <div
             className={cn(
@@ -67,6 +73,27 @@ export const DesignConfiguration = ({
             />
           </div>
         </Rnd>
+      </div>
+
+      <div className="h-[37.rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <RadioGroup value></RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
