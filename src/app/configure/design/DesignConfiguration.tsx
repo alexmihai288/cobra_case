08594b27321c0238d15d655edab78d74e4935817
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-13 21:12:36 +0300

    chore -m colors ddone

--- src/app/configure/design/DesignConfiguration.tsx
+++ src/app/configure/design/DesignConfiguration.tsx
@@ -7,6 +7,8 @@ import { Rnd } from "react-rnd";
 import { HandleComponent } from "@/components/HandleComponent";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { RadioGroup } from "@headlessui/react";
+import { COLORS } from "@/validators/option-validator";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfigurationProps {
   configId: string;
@@ -19,9 +21,9 @@ export const DesignConfiguration = ({
   imageUrl,
   imageDimensions,
 }: DesignConfigurationProps) => {
-  const [options,setOptions] = useState({
-    color:"black"
-  })
+  const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
+    color: COLORS[0],
+  });
 
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
@@ -43,7 +45,7 @@ export const DesignConfiguration = ({
           <div
             className={cn(
               "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
-              `bg-zinc-950`
+              `bg-${options.color.tw}`
             )}
           />
         </div>
@@ -90,7 +92,40 @@ export const DesignConfiguration = ({
             <div className="w-full h-px bg-zinc-200 my-6" />
 
             <div className="relative mt-4 h-full flex flex-col justify-between">
-              <RadioGroup value></RadioGroup>
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: val,
+                  }));
+                }}
+              >
+                <Label>Color:{options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <RadioGroup.Option
+                      key={color.label}
+                      value={color}
+                      className={({ active, checked }) =>
+                        cn(
+                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                          {
+                            [`border-${color.tw}`]: active || checked,
+                          }
+                        )
+                      }
+                    >
+                      <span
+                        className={cn(
+                          `bg-${color.tw}`,
+                          "h-8 w-8 rounded-full border border-black border-opacity-10"
+                        )}
+                      />
+                    </RadioGroup.Option>
+                  ))}
+                </div>
+              </RadioGroup>
             </div>
           </div>
         </ScrollArea>
