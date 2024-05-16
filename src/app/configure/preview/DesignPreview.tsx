c0f2e6c6e27aa3e6bf1e20ece8ccc297d7248e0d
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-16 21:43:07 +0300

    chore - design preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -3,6 +3,7 @@ import { Phone } from "@/components/Phone";
 import { cn } from "@/lib/utils";
 import { COLORS, MODELS } from "@/validators/option-validator";
 import { Configuration } from "@prisma/client";
+import { Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
 
@@ -41,6 +42,37 @@ export const DesignPreview = ({
             className={cn(`bg-${tw}`)}
             imgSrc={configuration.croppedImageUrl!}
           />
+        </div>
+
+        <div className="mt-6 sm:col-span-9 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <div className="font-medium text-zinc-950">Highlights</div>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>TPU shock absorption</li>
+                <li>Packaging made from recycled materials</li>
+                <li>5 yeat print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High-quality, durable material</li>
+                <li>Scratch and fingerprint resistan coating</li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
     </>
