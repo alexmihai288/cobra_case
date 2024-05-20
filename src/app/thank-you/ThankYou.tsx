7f9f6babdb8d7bfe974c2c1470a08acbfcb1d8b9
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-20 22:53:04 +0300

    chore - thank you page states

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -1,6 +1,18 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { getPaymentStatus } from "./actions";
+import { useSearchParams } from "next/navigation";
 
-export const ThankYou = () => {
+export const ThankYou = async () => {
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId") || "";
+
+  const { data } = useQuery({
+    queryKey: ["get-payment-status"],
+    queryFn: async () => await getPaymentStatus({ orderId }),
+    retry: true,
+    retryDelay: 500,
+  });
   return <div>ThankYou</div>;
 };
