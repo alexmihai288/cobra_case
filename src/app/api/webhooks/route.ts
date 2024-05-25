e4af4d6b7f67b383042af2ce5ab3beb17d90d0fc
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-25 20:55:02 +0300

    chore - final pro

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -33,7 +37,7 @@ export async function POST(req: Request) {
       const billingAddress = session.customer_details!.address;
       const shippingAdress = session.shipping_details!.address;
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -60,6 +64,25 @@ export async function POST(req: Request) {
             },
           },
         },
+      });
+
+      await resend.emails.send({
+        from: "CaseCobra <moldovanalexismihai06@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAdress!.city!,
+            country: shippingAdress!.country!,
+            postalCode: shippingAdress!.postal_code!,
+            street: shippingAdress!.line1!,
+            state: shippingAdress!.state!,
+          },
+        }),
       });
     }
 
